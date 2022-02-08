afae7f5dc1514c9130d2440da9f6f6f5204926ef
lekan <lekan.lekan@gmail.com>
2022-02-08 20:04:23 +0300

    login v.1.0

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -6,15 +6,14 @@ import (
 	"encoding/json"
 	"errors"
 	"fmt"
-	"github.com/gorilla/sessions"
+	"github.com/lekan/gophermart/internal/logger"
 	"github.com/lekan/gophermart/internal/models"
-	"log"
+	"github.com/lekan/gophermart/internal/sessions"
 	"net/http"
-	"os"
 	"time"
 )
 
-var store = sessions.NewCookieStore([]byte(os.Getenv("token_password")))
+var log = logger.GetLogger()
 
 func Signup(w http.ResponseWriter, r *http.Request) {
 	ctx, cancel := context.WithTimeout(r.Context(), 2*time.Second)
@@ -24,7 +23,7 @@ func Signup(w http.ResponseWriter, r *http.Request) {
 
 	// получаем Body
 	if err := json.NewDecoder(r.Body).Decode(creds); err != nil {
-		log.Println("JSON error")
+		log.Err(err).Msg("JSON error")
 		w.WriteHeader(http.StatusBadRequest)
 		return
 	}
@@ -33,26 +32,27 @@ func Signup(w http.ResponseWriter, r *http.Request) {
 	err := models.Signup(ctx, creds)
 	if err != nil {
 		if errors.Is(err, fmt.Errorf("409 %w", err)) {
+			log.Info().Msg("Login is in use another user")
 			w.WriteHeader(http.StatusConflict)
 			return
 		}
-		log.Println("Signup error")
+		log.Err(err).Msg("Signup error")
 		w.WriteHeader(http.StatusInternalServerError)
 	}
 
 	// создаем сессию
-	session, err := store.Get(r, "session_token")
+	session, err := sessions.Get(r)
 	if err != nil {
-		log.Println("Session error")
+		log.Err(err).Msg("Session error")
 		w.WriteHeader(http.StatusInternalServerError)
 		return
 	}
 
 	// записываем токен в сессию и сохраняем сессию
-	session.Values[creds.Login] = creds.Login
+	session.Values["token"] = creds.Login
 	err = session.Save(r, w)
 	if err != nil {
-		log.Println("Save session error")
+		log.Err(err).Msg("Save session error")
 		w.WriteHeader(http.StatusInternalServerError)
 		return
 	}
@@ -67,18 +67,41 @@ func Signin(w http.ResponseWriter, r *http.Request) {
 
 	creds := &models.Credentials{}
 
+	//получаем body
 	if err := json.NewDecoder(r.Body).Decode(creds); err != nil {
+		log.Err(err).Msg("json error")
 		w.WriteHeader(http.StatusBadRequest)
 		return
 	}
 
+	// ищем в базе данных
 	err := models.Signin(ctx, creds)
 	if err != nil {
 		if errors.Is(err, sql.ErrNoRows) {
+			log.Info().Msg("user does not exist")
 			w.WriteHeader(http.StatusUnauthorized)
 			return
 		}
+		log.Err(err)
 		w.WriteHeader(http.StatusInternalServerError)
+		return
 	}
 
+	session, err := sessions.Get(r)
+	if err != nil {
+		log.Err(err).Msg("Session error")
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	session.Values["token"] = creds.Login
+	err = session.Save(r, w)
+	if err != nil {
+		log.Err(err).Msg("save session error")
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Add("Context-Type", "application/json")
+	w.WriteHeader(200)
 }
