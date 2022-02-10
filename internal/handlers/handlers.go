b9eee89b34d711e7b5a869ba3201e321ba8095e2
lekan <lekan.lekan@gmail.com>
2022-02-11 00:21:47 +0300

    login v.1.6

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -247,15 +247,15 @@ func GetOrders(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
 }
 
-func Withdraw(w http.ResponseWriter, r *http.Request) {
-	ctx, cancel := context.WithTimeout(r.Context(), 2*time.Second)
-	defer cancel()
-
-	session, err := sessions.Get(r)
-	if err != nil {
-		log.Err(err).Msg("Session error")
-		w.WriteHeader(http.StatusInternalServerError)
-		return
-	}
-
-}
+//func Withdraw(w http.ResponseWriter, r *http.Request) {
+//	ctx, cancel := context.WithTimeout(r.Context(), 2*time.Second)
+//	defer cancel()
+//
+//	session, err := sessions.Get(r)
+//	if err != nil {
+//		log.Err(err).Msg("Session error")
+//		w.WriteHeader(http.StatusInternalServerError)
+//		return
+//	}
+//
+//}
