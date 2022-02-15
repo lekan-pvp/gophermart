7e36d44fcd750030cf103f017f315131788a0e05
lekan <lekan.lekan@gmail.com>
2022-02-15 23:00:25 +0300

    login v.7.8

--- internal/models/accounts.go
+++ internal/models/accounts.go
@@ -194,7 +194,7 @@ func PostOrder(ctx context.Context, login string, orderId []byte) (int, error) {
 			return http.StatusInternalServerError, err
 		}
 	} else {
-		_, err = db.ExecContext(ctx, `UPDATE orders SET status=$1, uploaded_at=$2 WHERE order_id=$4 AND username=$5`, order.Status, time.Now().Format(time.RFC3339), order.OrderId, login)
+		_, err = db.ExecContext(ctx, `UPDATE orders SET status=$1, uploaded_at=$2 WHERE order_id=$3 AND username=$4`, order.Status, time.Now().Format(time.RFC3339), order.OrderId, login)
 		if err != nil {
 			log.Err(err).Msg("database update error")
 			return http.StatusInternalServerError, err
